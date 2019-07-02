04ab604f3076b3a8e6d6bba6c85864f2a798d868
Ralph <ralph.lee@live.ca>
2019-07-02 23:08:54 +0000

    Added alarm beep function

--- timer1.js
+++ timer1.js
@@ -1,6 +1,8 @@
 const alarm = process.argv.slice(2);
 
-for (let time in alarm) {
-  setTimeout(function() 
-  {process.stdout.write('\x07')}, time * 1000)
+for (let time of alarm) {
+  if (time > 0) {
+    setTimeout(function() 
+    {process.stdout.write('\x07')}, time * 1000)
+  }
 };
